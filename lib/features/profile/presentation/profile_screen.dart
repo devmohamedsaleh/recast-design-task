46906bb54e60a7f85c192c85d67eb8e2c3148bfd
AMIT <za.mohamedsaleh011213@gmail.com>
2026-03-11 03:06:26 +0200

    add_project_screenshots

--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -7,6 +7,7 @@ import 'widgets/profile_header.dart';
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
