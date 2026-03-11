ac09e55d22a6ee734c2191dce145587da4c3e463
AMIT <za.mohamedsaleh011213@gmail.com>
2026-03-11 02:05:02 +0200

    handle safe area in home and profile screens

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -16,6 +16,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
+
       create: (_) => HomeCubit(),
       child: const _HomeView(),
     );
